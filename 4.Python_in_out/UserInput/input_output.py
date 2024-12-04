017565003332b31921a57b1a9f5463bf3b626f0c
insung <in27sung@gmail.com>
2024-12-04 15:11:09 +0900

    Learn input_output.py

---
+++ 4.Python_in_out/UserInput/input_output.py
@@ -0,0 +1,21 @@
+
+# 사용자 입력과 출력
+
+# 사용자 입력
+# 사용자가 입력한 값을 변수에 대입할 때 input() 함수 사용
+a = input()
+print(a)
+
+# 프롬프트 값을 띄워서 사용자 입력 받기
+number = input("숫자를 입력하세요: ")
+print(number)
+
+# 큰 따옴표(")로 둘러싸인 문자열은 + 연산과 동일하다
+print("life""is""too short")
+# -> lifeisToo short
+print("life" + "is" + "too short")
+# -> lifeisToo short
+
+#  문자열의 띄어쓰기는 콤마로 한다.
+print("life","is","too short")
+# ->life is too short
